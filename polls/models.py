17d467542398c0a96b76dc8b16e871880b309ce0
daisel10 <davidsanchezlombana10@gmail.com>
2023-05-23 21:44:27 -0500

    add Polls

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    # id 
+    name = models.CharField(max_length=10, unique=True)
+    gmail = models.EmailField()
+    description = models.CharField(max_length=200)
